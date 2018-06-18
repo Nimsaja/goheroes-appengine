dbd4c56941de5f71a09180a65fabfae0005394a2
lima1909 <lima1909@github.com>
2018-06-18 23:06:03 +0200

    remove endpoints

--- server.go
+++ server.go
@@ -3,31 +3,36 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"strconv"
 	"strings"
 
-	"github.com/gorilla/mux"
 	"github.com/lima1909/goheroes-appengine/db"
+	"github.com/lima1909/goheroes-appengine/service"
+
+	"github.com/gorilla/mux"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 var (
+	svc service.HeroService
 	// Heroes list from Hero examples
-	Heroes = []db.Hero{
-		db.Hero{ID: "1", Name: "Jasmin"},
-		db.Hero{ID: "2", Name: "Mario"},
-		db.Hero{ID: "3", Name: "Alex M"},
-		db.Hero{ID: "4", Name: "Adam O"},
-		db.Hero{ID: "5", Name: "Shauna C"},
-		db.Hero{ID: "6", Name: "Lena H"},
-		db.Hero{ID: "7", Name: "Chris S"},
+	Heroes = []service.Hero{
+		service.Hero{ID: 1, Name: "Jasmin"},
+		service.Hero{ID: 2, Name: "Mario"},
+		service.Hero{ID: 3, Name: "Alex M"},
+		service.Hero{ID: 4, Name: "Adam O"},
+		service.Hero{ID: 5, Name: "Shauna C"},
+		service.Hero{ID: 6, Name: "Lena H"},
+		service.Hero{ID: 7, Name: "Chris S"},
 	}
 )
 
 func main() {
+
+	svc = db.DataStoreService{}
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/heroes", heroes)
@@ -36,7 +41,7 @@ func main() {
 
 	http.Handle("/", router)
 
-	log.Println("Start Server: http://localhost:8081")
+	// log.Println("Start Server: http://localhost:8081")
 	// log.Fatalln(http.ListenAndServe(":8081", nil))
 	appengine.Main()
 }
@@ -103,6 +108,7 @@ func getHeroByID(w http.ResponseWriter, r *http.Request) {
 		fireError(w, "Err by marshal hero: %v", err)
 		return
 	}
+	log.Infof(appengine.NewContext(r), "Names=%v -> Name=%v", names, name)
 
 	fmt.Fprintf(w, string(b))
 }
@@ -110,13 +116,14 @@ func getHeroByID(w http.ResponseWriter, r *http.Request) {
 func updateHero(w http.ResponseWriter, r *http.Request) {
 	hero, err := getHeroFromRequest(r, w)
 
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	heroes, err := svc.List(appengine.NewContext(r), name)
 	if err != nil {
-		fireError(w, "Err by getHeroFromRequest %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	i, err := strconv.Atoi(hero.ID)
-
+	b, err := json.Marshal(heroes)
 	if err != nil {
 		fireError(w, "Err during string convert: %v", err)
 		return
@@ -138,9 +145,105 @@ func addHero2(w http.ResponseWriter, r *http.Request) {
 
 	hero.ID = strconv.Itoa(len(Heroes) + 1)
 
-	Heroes = append(Heroes, hero)
+	// func heros(w http.ResponseWriter, r *http.Request) {
 
-	b, err := json.Marshal(hero)
+	// 	switch r.Method {
+	// 	case "GET":
+	// 		// loadHeroes(w)
+	// 		h, err := db.ListHeroes(appengine.NewContext(r))
+	// 		if err != nil {
+	// 			fmt.Fprintf(w, "%v", err)
+	// 		}
+	// 		fmt.Fprintln(w, toJSON(h))
+	// 	case "OPTIONS":
+	// 		writeToClient(w, string(http.StatusOK))
+	// 	case "PUT":
+	// 		updateHero(w, r)
+	// 	case "POST":
+	// 		addHero(w, r)
+	// 	}
+	// }
+
+	// func toJSON(v interface{}) string {
+	// 	b, err := json.Marshal(v)
+
+	// 	if err != nil {
+	// 		return fmt.Sprintf("Err by marshal heros: %v", err)
+	// 	}
+
+	// 	return string(b)
+	// }
+
+	// func loadHeroes(w http.ResponseWriter) {
+	// 	b, err := json.Marshal(Heroes)
+
+	// 	if err != nil {
+	// 		fmt.Fprintf(w, "Err by marshal heros: %v", err)
+	// 		return
+	// 	}
+
+	// 	writeToClient(w, string(b))
+	// }
+
+	// func getHeroByID(w http.ResponseWriter, r *http.Request) {
+	// 	vars := mux.Vars(r)
+	// 	varID := vars["id"]
+	// 	i, err := strconv.Atoi(varID)
+
+	// 	if err != nil {
+	// 		fmt.Fprintf(w, "Err during string convert: %v", err)
+	// 		return
+	// 	}
+
+	// 	b, err := json.Marshal(Heroes[i-1])
+
+	// 	if err != nil {
+	// 		fmt.Fprintf(w, "Err by marshal hero: %v", err)
+	// 		return
+	// 	}
+
+	// 	writeToClient(w, string(b))
+	// }
+
+	// func updateHero(w http.ResponseWriter, r *http.Request) {
+	// 	hero, err := getHeroFromRequest(r, w)
+
+	// 	if err != nil {
+	// 		fmt.Fprintf(w, "Err by getHeroFromRequest %v", err)
+	// 		return
+	// 	}
+
+	// 	i, err := strconv.Atoi(hero.ID)
+
+	// 	if err != nil {
+	// 		fmt.Fprintf(w, "Err during string convert: %v", err)
+	// 		return
+	// 	}
+
+	// 	//update Hero in List
+	// 	Heroes[i-1] = hero
+
+	// 	writeToClient(w, "")
+	// }
+
+	// func addHero2(w http.ResponseWriter, r *http.Request) {
+	// 	hero, err := getHeroFromRequest(r, w)
+
+	// 	if err != nil {
+	// 		fmt.Fprintf(w, "Err by getHeroFromRequest %v", err)
+	// 		return
+	// 	}
+
+	// 	hero.ID = strconv.Itoa(len(Heroes) + 1)
+
+	// 	Heroes = append(Heroes, hero)
+
+	// 	b, err := json.Marshal(hero)
+
+	// 	if err != nil {
+	// 		fmt.Fprintf(w, "Err by marshal hero: %v", err)
+	// 		return
+	// 	}
 
 	if err != nil {
 		fireError(w, "Err by marshal hero: %v", err)
@@ -201,25 +304,21 @@ func searchHeroes(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(b))
 }
 
-func getHeroFromRequest(r *http.Request, w http.ResponseWriter) (db.Hero, error) {
-	//read transfered data
-	body, err := ioutil.ReadAll(r.Body)
-	defer r.Body.Close()
+// 	//convert to Hero
+// 	var hero db.Hero
+// 	err = json.Unmarshal(body, &hero)
 
-	if err != nil {
-		return db.Hero{}, err
-	}
+// 	if err != nil {
+// 		return db.Hero{}, err
+// 	}
 
-	//convert to Hero
-	var hero db.Hero
-	err = json.Unmarshal(body, &hero)
+// 	return hero, nil
+// }
 
-	if err != nil {
-		return db.Hero{}, err
-	}
-
-	return hero, nil
-}
+// func writeToClient(w http.ResponseWriter, s string) {
+// 	w.Header().Set("Access-Control-Allow-Origin", "*")
+// 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+// 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 func setHeaderOptions(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
